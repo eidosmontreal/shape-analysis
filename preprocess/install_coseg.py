9abe5352bad16bdbe7184f5f27273fa1798b0beb
Bilal Abbasi <bilalabbasi@192.168.1.115>
2022-05-01 19:27:15 +0100

    adds scripts for downloading and preprocessing data

--- preprocess/install_coseg.py
+++ preprocess/install_coseg.py
@@ -1,14 +1,59 @@
-import subprocess, argparse
+import argparse
+from typing import NoReturn
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--data-dir", type=str, required=True, help='Directory where data is stored')
-parser.add_argument("--ratio", type=float, default=0.85, required=False, help='Ratio of train to test data')
-args = parser.parse_args()
+import coseg
+import yaml
 
-subprocess.call(['bash coseg/download_data.sh {}'.format(args.data_dir)],shell=True)
-print('Splitting test and train data...')
-subprocess.call(['python coseg/split_train_test.py {} {}'.format(args.data_dir, args.ratio)],shell=True)
-print('Converting face labels to vertex labels...')
-subprocess.call(['python coseg/convert_labels.py {}'.format(args.data_dir)],shell=True)
-print('...done!')
+config = yaml.safe_load(open("config.yml"))
 
+
+def install_coseg(data_dir: str, ratio: float = 0.85, splits: str = None) -> NoReturn:
+    """
+    Downloads and installs the COSEG dataset.
+    The script does the following:
+        - Download the COSEG dataset
+        - Split the data into a training and testing set
+        - Transform the face labels to vertex labels
+
+    :param data_dir: Directory where data will be stored and processed
+    :param ratio: Percentage of data to be converted to training (remaining is test)
+    """
+    print("Downloading data...")
+    coseg.download_data(args.data_dir)
+    print("...done!")
+
+    print("Splitting test and train data...")
+    coseg.split_train_test(args.data_dir, args.ratio, args.splits)
+    print("...done!")
+
+    print("Converting face labels to vertex labels...")
+    coseg.convert_labels(args.data_dir)
+    print("...done!")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data-dir",
+        type=str,
+        default=config["data_dir"]["coseg"],
+        required=False,
+        help="Directory where data is stored",
+    )
+    parser.add_argument(
+        "--ratio",
+        type=float,
+        default=0.85,
+        required=False,
+        help="Ratio of train to test data",
+    )
+    parser.add_argument(
+        "--splits",
+        type=str,
+        default=None,
+        required=False,
+        help="yaml containing train/test splits",
+    )
+    args = parser.parse_args()
+
+    install_coseg(args.data_dir, args.ratio, args.splits)

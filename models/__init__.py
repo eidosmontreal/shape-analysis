f5331e5da0a821fa9b359b301ec411ad93dc0a08
Bilal Abbasi <bilalabbasi@192.168.1.126>
2022-05-08 14:54:17 +0100

    removes scripts and models related to pooling

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,3 @@
-from .architectures import (AutoEncoder, LinearMetricNet, MetricConvNet,
-                            MetricResNet)
-from .mesh_pool import MeshPool
+from .architectures import LinearMetricNet, MetricConvNet, MetricResNet
 from .metric import *
 from .metric_conv import MetricConv
